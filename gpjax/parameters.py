f85be69110d31f65e769948d7b897953694e4496
Daniel-Dodd <daniel_dodd@icloud.com>
2022-06-16 15:07:04 +0100

    Minimal working natural gradient functions for NAT PARAMETERISATION.

--- gpjax/parameters.py
+++ gpjax/parameters.py
@@ -44,7 +44,7 @@ def initialise(model, key: PRNGKeyType = None, **kwargs) -> ParameterState:
         for k, v in kwargs.items():
             params[k] = merge_dictionaries(params[k], v)
     constrainers, unconstrainers = build_transforms(params)
-    trainables = build_trainables(params)
+    trainables = build_trainables_true(params)
     state = ParameterState(
         params=params,
         trainables=trainables,
@@ -52,7 +52,6 @@ def initialise(model, key: PRNGKeyType = None, **kwargs) -> ParameterState:
         unconstrainers=unconstrainers,
     )
     return state
-
 
 def _validate_kwargs(kwargs, params):
     for k, v in kwargs.items():
@@ -274,7 +273,7 @@ def prior_checks(priors: dict) -> dict:
     return priors
 
 
-def build_trainables(params: tp.Dict) -> tp.Dict:
+def build_trainables_true(params: tp.Dict) -> tp.Dict:
     """Construct a dictionary of trainable statuses for each parameter. By default, every parameter within the model is trainable.
 
     Args:
@@ -287,6 +286,22 @@ def build_trainables(params: tp.Dict) -> tp.Dict:
     prior_container = deepcopy(params)
     # Set all values to zero
     prior_container = jax.tree_util.tree_map(lambda _: True, prior_container)
+    return prior_container
+
+
+def build_trainables_false(params: tp.Dict) -> tp.Dict:
+    """Construct a dictionary of trainable statuses for each parameter. By default, every parameter within the model is NOT trainable.
+
+    Args:
+        params (tp.Dict): The parameter set for which trainable statuses should be derived from.
+
+    Returns:
+        tp.Dict: A dictionary of boolean trainability statuses. The dictionary is equal in structure to the input params dictionary.
+    """
+    # Copy dictionary structure
+    prior_container = deepcopy(params)
+    # Set all values to zero
+    prior_container = jax.tree_map(lambda _: False, prior_container)
     return prior_container
 
 
